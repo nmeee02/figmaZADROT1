d15cda80cd0145245937d1581a60c7e1ca1b9e25
nme <nme@DESKTOP-CTCFIIF>
2024-04-24 12:15:31 +0300

    Добавьте файлы проекта.

---
+++ figmaZADROT/MyProfile.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace figmaZADROT
+{
+    /// <summary>
+    /// Логика взаимодействия для MyProfile.xaml
+    /// </summary>
+    public partial class MyProfile : Page
+    {
+        public MyProfile()
+        {
+            InitializeComponent();
+        }
+
+        internal void Show()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        private void myback(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ figmaZADROT/chats.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace figmaZADROT
+{
+    /// <summary>
+    /// Логика взаимодействия для chats.xaml
+    /// </summary>
+    public partial class chats : Window
+    {
+        public chats()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            allthis.Content = new MyProfile();        
+        }
+
+        private void ListRazdelov_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
